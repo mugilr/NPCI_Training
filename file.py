1688a54a1e3efcb2b4ced3f18cff0588729fc5c0
Mugil <dmugil2000@gmail.com>
2021-09-29 18:07:26 +0530

    Day 3 Assingment 3

---
+++ file.py
@@ -0,0 +1,32 @@
+import abc
+
+
+lst = [['Ram','ram@ab.com',12345],
+        ['Som','som@ab.com',67890],
+        [],
+        ['Khan','khan@ab.com',12315,'Hyd'],
+        ['Ram','ram@ab.com',12345,'cdm','698291']
+        ]
+
+title = ['Name','Mail','Phone']
+f = open('file.txt','w')
+f.write('')
+f.close()
+f = open('file.txt','a+',)
+f1 = open('file.txt','r',)
+
+for i in title:
+    f.write(str(i))
+    f.write(',')
+
+print(f.tell())
+for i in lst:
+    if len(i)>=3:
+        f.write('\n')
+        for j in range(0,3,1):
+            f.write(str(i[j]))
+            f.write(',')
+f.close()
+
+# re = f1.read()
+# print(re)
