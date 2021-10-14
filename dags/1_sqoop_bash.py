2f39ee2c07d86eef39d69483ce6b33e9e31e6f18
Mugilraj <dmugil2000@gmail.com>
2021-10-14 13:18:20 +0530

    added incline comments

--- dags/1_sqoop_bash.py
+++ dags/1_sqoop_bash.py
@@ -1,4 +1,5 @@
 
+#Required modules to import
 from datetime import timedelta
 from airflow import DAG
 from airflow.operators.bash import BashOperator
@@ -8,27 +9,36 @@ from airflow.operators.dummy import DummyOperator
 from airflow.utils.dates import days_ago
 
 
+#Fuction which will be called when the task which resolve the error succeded 
+#this fuction will be used to re-run whole dag again
 def clear_ts(context):
     ex_time = context.get('execution_date')
     print("##################### Callback #################")
+#this BashOperator task will clear the running dag status to null. So, that
+#the airflow scheduler will re-schedule the tasks.
     clear_task = BashOperator(
             task_id = 'clear_task',
+#Here, -s flag for execution/start data
+#      -u include upstream task
+#      -d include downsteam task
+#      -y for confirm 'yes'
             bash_command = f'airflow tasks clear -s {ex_time} -u -d -y 1_sqoop_import'
             )
     return clear_task.execute(context=context)
 
+#This is a PythonBranch operator which can be used to choose which task needs to
+#upstream based on the condition
 def check(ti):
+#Pull the error from the xcom 
     val = ti.xcom_pull(key='error', task_ids = '1_sqoop_import')
     
     print('########  Error $$$$ {0}  $$$$$   #######'.format(val))
+#Check for the error exception 
     if (val.find('FileAlreadyExistsException') !=-1):
+#return the task id which needs to upstream
         return 'rm_hdfs'
     else:
         return 'sus'
-
-
-
-
 
 args = {
     'owner': 'airflow',
@@ -65,11 +75,13 @@ rm_hdfs = BashOperator(
         dag=dag,
         )
 
+	'''
 err_ts = BashOperator(
         task_id = 'err_ts',
         bash_command = 'exit 99',
         on_failure_callback = clear_ts,
         )
+'''
 
 sus = BashOperator(
         task_id = 'sus',
